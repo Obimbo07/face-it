623d7fa667cd14d7bb285974cbbf902d32fb946c
Obimbo07 <obimboausts@gmail.com>
2024-07-26 23:01:38 +0300

    Feature: changed the application from flask to FastApi

--- app/face_verification.py
+++ app/face_verification.py
@@ -1,16 +1,14 @@
+# face_verification.py
 import cv2
-from deepface import DeepFace
-import matplotlib.pyplot as plt
 import numpy as np
+from deepface import DeepFace
 
-def load_image_with_opencv(image_file):
-    # Read the image file with OpenCV
-    image = cv2.imdecode(np.frombuffer(image_file.read(), np.uint8), cv2.IMREAD_COLOR)
-    # Convert BGR image (OpenCV default) to RGB (DeepFace default)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+def load_image_with_opencv(file):
+    # Read image file
+    image = cv2.imdecode(np.frombuffer(file.read(), np.uint8), cv2.IMREAD_COLOR)
     return image
 
 def verify_faces(known_image, compared_image):
-    # Verify the faces using DeepFace with image arrays
-    result = DeepFace.verify(img1_path=known_image, img2_path=compared_image)
-    return result
+    # Perform face verification
+    result = DeepFace.verify(known_image, compared_image)
+    return result["verified"]
